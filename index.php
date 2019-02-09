c668acf3ce5032e888bb5bc89fd5a9deae1e1ca3
sureshchandrarhca15 <sureshchandra.rhca@gmail.com>
2019-02-09 16:16:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
  <?php 
- echo "I am VERSION 3 with AB Test Deployment <br><br>";
+ echo "I am VERSION 3 with AB Test Deployment of app-c <br><br>";
  echo "My Pod IP is : ".$_SERVER['SERVER_ADDR'] ;
  ?>
